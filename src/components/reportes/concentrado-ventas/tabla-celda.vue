b1555b8133c5f8b6d8ef28bbec3f574c89911c67
Mauricio hernandez <mhernandez@turningtechnologies.com>
2020-10-17 08:35:21 -0500

    update cells

--- src/components/reportes/concentrado-ventas/tabla-celda.vue
+++ src/components/reportes/concentrado-ventas/tabla-celda.vue
@@ -1,20 +1,17 @@
 <template>
 <div>
-  <div class="panel" v-if="typeof value == 'object'">
-    <div class="panel-container">
-        <div class="real w-50">
-          <small>Real:</small>
-          <div>
-            {{value.real | number}}
-          </div>
-        </div>
-        <div class="meta w-50">
-          <small>Meta:</small>
-          <div>
-            {{value.meta | number}}
-          </div>
-        </div>
-        <div class="porcentaje w-100 text-darken-4" :class="{'red-text': porcentaje < 80, 'green-text': porcentaje > 80}">{{porcentaje}} %</div>
+  <div class="cell-container" v-if="typeof value == 'object'">
+    <div class="cell">
+      <span class="cell-name">REAL:</span>
+      <span class="cell-value">{{value.real | number}}</span>
+    </div>
+    <div class="cell">
+      <span class="cell-name">META:</span>
+      <span class="cell-value">{{value.meta | number}}</span>
+    </div>
+    <div class="cell">
+      <span class="cell-name">%:</span>
+      <span class="cell-value" :class="getClass(porcentaje)">{{value.porcentaje | number}} %</span>
     </div>
   </div>
   <div class="center" v-if="typeof value != 'object'">
@@ -25,6 +22,17 @@
 <script>
 export default {
   props: ['value'],
+  methods: {
+    getClass(porcentaje) {
+      if(porcentaje > 90){
+        return 'buemo';
+      }
+      if(porcentaje > 60){
+        return 'regular';
+      }
+      return 'malo';
+    },
+  },
   computed: {
     porcentaje() {
       if (typeof this.value == 'object') {
@@ -40,28 +48,28 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  small{
-    font-size: .9rem;
-  }
-  .panel {
-    padding: 0;
-    margin-bottom: 0;
-    font-size: 1.1rem;
-  }
-  .panel-container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .w-50 {
-    width: 50%;
-  }
-  .w-100 {
-    width: 100%;
-  }
-  .real {
-    border-right: 1px solid #CCC;
-  }
-  .porcentaje {
-    border-top: 1px solid #CCC;
+  .cell-container {
+    width: 90%;
+    text-align: center;
+    .cell {
+      padding: 0.5rem 1rem;
+      display: flex;
+      justify-content: space-between;
+      border-radius: 1rem;
+      color: #444444;
+      font-family: "axiforma bold";
+      font-weight: bold;
+      background: #FFF;
+      margin-bottom: .5rem;
+      .bueno {
+        color: #84d0c1;
+      }
+      .malo {
+        color: #f5b0b6;
+      }
+      .regular {
+        color: #ecd9a4;
+      }
+    }
   }
 </style>
